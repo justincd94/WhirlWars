d4afd652581f6f3b489dc36311bb514fcb0ff583
Justin DeSimone <jdesimone32894@gmail.com>
2018-12-13 15:18:10 -0500

    fixed music overlap

--- Whirl Wars/Assets/Scripts/ContinuousMusic.cs
+++ Whirl Wars/Assets/Scripts/ContinuousMusic.cs
@@ -21,7 +21,7 @@ public class ContinuousMusic : MonoBehaviour {
     {
         Scene scene = SceneManager.GetActiveScene();
 
-        if (scene.buildIndex == 4 || scene.buildIndex == 5)
+        if (scene.buildIndex == 4 || scene.buildIndex == 5 || scene.buildIndex == 6)
         {
             Destroy(this.gameObject);
         }
